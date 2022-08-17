d2a1a4cd9f8e84c43ed55427dbc515a53c2123ad
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2022-08-18 02:05:11 +0530

    commit

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-class QuizQuestionButton extends Component{
-    constructor(props){
-        super(props)
-    }
-    handleClick(){
-      this.props.clickHandler(this.props)
-    }
-  render(){
-    return(
-        <ul>
-            <li>
-                <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
-            </li>
-        </ul>
+class QuizQuestionButton extends Component {
+  handleClick() {
+    this.props.clickHandler(this.props.button_text)
+  }
+  render() {
+    return (
+      <li>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
+      </li>
     )
   }
 }
 
-export default QuizQuestionButton;
+export default QuizQuestionButton
