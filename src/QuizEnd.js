8a3776169659f23df58a6293f44db9f9eb99f769
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2022-08-18 01:52:59 +0530

    commit

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class QuizEnd extends Component {
-  render() {
-    return (
-      <div>
+class QuizEnd extends Component{
+ render(){
+    return(
+        <div>
         <p>Thanks for playing!</p>
         <a href=''>Reset Quiz</a>
-      </div>      
+      </div>
     )
-  }
+ }
 }
 
-export default QuizEnd
+export default QuizEnd;
